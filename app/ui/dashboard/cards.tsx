631b215ddc0ab6c266884d381c7c31f0b48ab2d1
Kiyo-Shikami <KiyoShikamiii@gmail.com>
2024-07-01 22:57:39 +0800

    after test -deleted 3 test file to learn nextjs

--- app/ui/dashboard/cards.tsx
+++ app/ui/dashboard/cards.tsx
@@ -34,10 +34,12 @@ export function Card({
   title,
   value,
   type,
+  number,
 }: {
   title: string;
   value: number | string;
-  type: 'invoices' | 'customers' | 'pending' | 'collected';
+    type: 'invoices' | 'customers' | 'pending' | 'collected';
+    number: string;
 }) {
   const Icon = iconMap[type];
 
@@ -45,7 +47,7 @@ export function Card({
     <div className="rounded-xl bg-gray-50 p-2 shadow-sm">
       <div className="flex p-4">
         {Icon ? <Icon className="h-5 w-5 text-gray-700" /> : null}
-        <h3 className="ml-2 text-sm font-medium">{title}</h3>
+        <h3 className="ml-2 text-sm font-medium">{title}{number}</h3>
       </div>
       <p
         className={`${lusitana.className}
